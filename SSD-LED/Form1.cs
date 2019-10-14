469a98dd78b3ad337b0beccba818147fb5f09caf
SIRprise <peter3@turboprinz.de>
2019-10-14 15:45:11 +0200

    fix hide/unhide form + code cleanup

--- SSD-LED/Form1.cs
+++ SSD-LED/Form1.cs
@@ -12,19 +12,6 @@ namespace SSD_LED
 {
     public partial class SSDLED : Form
     {
-        /*
-        [System.Runtime.InteropServices.DllImport("psapi.dll", CharSet = CharSet.Auto)]
-        extern static int EmptyWorkingSet(IntPtr hwProc);
-
-        static void MinimizeFootprint()
-        {
-            EmptyWorkingSet(Process.GetCurrentProcess().Handle);
-        }
-        */
-
-        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = CharSet.Auto)]
-        extern static bool DestroyIcon(IntPtr handle);
-
         private NotifyIcon notifyIcon;
         Icon iconBlack;
         //ManagementClass driveDataClass = new ManagementClass("Win32_PerfFormattedData_PerfDisk_PhysicalDisk");
@@ -81,6 +68,10 @@ namespace SSD_LED
             //MinimizeFootprint();
         }
 
+        #region icon stuff
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = CharSet.Auto)]
+        extern static bool DestroyIcon(IntPtr handle);
+
         private void initTrayIcon()
         {
             notifyIcon = new NotifyIcon();
@@ -110,27 +101,6 @@ namespace SSD_LED
             notifyIcon.Visible = false;
             notifyIcon.Icon = null;
             notifyIcon.Dispose();
-        }
-
-        private string NameAndVersion()
-        {
-            return System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + " v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
-        }
-
-        private void OnReadTimeOut(object sender, ElapsedEventArgs e)
-        {
-            readTimer.Enabled = false;
-            endReading = true;
-            readThread.Join();
-            readThread = null;
-            if (button4.InvokeRequired)
-            {
-                button4.Invoke((MethodInvoker)delegate { button4.Enabled = true; });
-            }
-            else
-            {
-                button4.Enabled = true;
-            }
         }
 
         private Icon CreateIcon(Color color)
@@ -182,32 +152,34 @@ namespace SSD_LED
 
             return icon;
         }
+        #endregion
 
-
-        void info_Click(object sender, EventArgs e)
+        #region controller / core logic
+        /*
+        public void SSDActivityWMI()
         {
-            this.Visible = true;
-            this.WindowState = FormWindowState.Normal;
-            this.BringToFront();
-        }
-
-        void exit_Click(object sender, EventArgs e)
-        {
-            this.Visible = true;
-            this.WindowState = FormWindowState.Normal;
-            this.BringToFront();
-            this.ShowInTaskbar = true;
-            timer1.Enabled = false;
-            /*
-            if (driveDataClass != null)
+            ManagementObjectCollection driveDataClassCollection = driveDataClass.GetInstances();
+            foreach (ManagementObject obj in driveDataClassCollection)
             {
-                driveDataClass.Dispose();
+                if (obj["Name"].ToString() == "_Total")
+                {
+                    Int64 bytesPS = Convert.ToInt64(obj["DiskBytesPersec"]);
+                    if (bytesPS > 100)
+                    {
+                        bytesPS /= 1024;
+                        bytesPS = bytesPS > 255 ? 255 : bytesPS;
+                        notifyIcon.Icon = CreateIcon(Color.FromArgb(0,(int)bytesPS,0));//iconGreen;
+                        notifyIcon.Text = bytesPS + " KB/s";
+                    }
+                    else
+                    {
+                        notifyIcon.Icon = iconBlack;
+                    }
+                }
+
             }
-             */
-            //Close();
-            removeTrayIcon();
-            Application.Exit();
         }
+         */
 
         public void SSDActivityPerfCount()
         {
@@ -277,6 +249,18 @@ namespace SSD_LED
             //}
             tickCount++;
         }
+        #endregion
+
+        #region helpers
+        /*
+        [System.Runtime.InteropServices.DllImport("psapi.dll", CharSet = CharSet.Auto)]
+        extern static int EmptyWorkingSet(IntPtr hwProc);
+
+        static void MinimizeFootprint()
+        {
+            EmptyWorkingSet(Process.GetCurrentProcess().Handle);
+        }
+        */
 
         float GetCounterValue(PerformanceCounter pc, string categoryName, string counterName, string instanceName)
         {
@@ -285,39 +269,84 @@ namespace SSD_LED
             pc.InstanceName = instanceName;
             return pc.NextValue();
         }
-
+        
         string GetInstanceNameByDriveIndex(int driveIndex)
         {
             PerformanceCounterCategory pfcCat = new PerformanceCounterCategory("PhysicalDisk");
             return pfcCat.GetInstanceNames()[driveIndex];
         }
 
-        /*
-        public void SSDActivityWMI()
+        private string NameAndVersion()
         {
-            ManagementObjectCollection driveDataClassCollection = driveDataClass.GetInstances();
-            foreach (ManagementObject obj in driveDataClassCollection)
+            return System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + " v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        private void ReadSpeed(string sDir)
+        {
+            //1st try: iterate through directories and read...
+            try
             {
-                if (obj["Name"].ToString() == "_Total")
+                foreach (string f in Directory.GetFiles(sDir))
                 {
-                    Int64 bytesPS = Convert.ToInt64(obj["DiskBytesPersec"]);
-                    if (bytesPS > 100)
+                    byte[] buf = new byte[1024 * 1024];
+                    try
                     {
-                        bytesPS /= 1024;
-                        bytesPS = bytesPS > 255 ? 255 : bytesPS;
-                        notifyIcon.Icon = CreateIcon(Color.FromArgb(0,(int)bytesPS,0));//iconGreen;
-                        notifyIcon.Text = bytesPS + " KB/s";
+                        int offset = 0;
+                        while (endReading != false)
+                        {
+                            using (Stream file = File.OpenRead(f))
+                            {
+                                file.Read(buf, offset, 1024 * 1024);
+                                offset += 1024 * 1024;
+                            }
+                        }
                     }
-                    else
-                    {
-                        notifyIcon.Icon = iconBlack;
-                    }
+                    catch (Exception) { };
+                    if (endReading)
+                        break;
                 }
 
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    if (endReading)
+                        break;
+                    ReadSpeed(d);
+                }
             }
-        }
-         */
+            catch (System.Exception) { };
 
+        }
+
+
+
+        private void RefreshDriveList()
+        {
+            comboBox1.Items.Clear();
+            /*
+            foreach (string logicalDrive in Environment.GetLogicalDrives)
+            {
+                DriveInfo driveInfo = new DriveInfo(logicalDrive);
+                if (driveInfo.IsReady)
+                {
+                    this.comboBox1.Items.Add((object)(logicalDrive.ToUpper() + " [" + driveInfo.DriveFormat.ToString() + "]"));//; " + (object)(driveInfo.TotalSize / 1024L / 1024L) + " / " + (object)(driveInfo.AvailableFreeSpace / 1024L / 1024L) + " MiB]"));
+                }
+            }
+             */
+            /*
+            foreach (System.IO.DriveInfo drive in System.IO.DriveInfo.GetDrives())
+            {
+                if (drive.DriveType == System.IO.DriveType.Fixed) 
+                {
+                    comboBox1.Items.Add(drive.Name.ToString());
+                }
+            }
+             */
+            PerformanceCounterCategory pfcCat = new PerformanceCounterCategory("PhysicalDisk");
+            comboBox1.Items.AddRange(pfcCat.GetInstanceNames());
+        }
+        #endregion
+
+        #region events
         private void timer1_Tick(object sender, EventArgs e)
         {
             //SSDActivityWMI();
@@ -341,10 +370,49 @@ namespace SSD_LED
             }
         }
 
+        void info_Click(object sender, EventArgs e)
+        {
+            //toggle
+            if (this.Visible == false)
+            {
+                //unhide
+                this.Visible = true;
+                //ensure it is really in front
+                this.WindowState = FormWindowState.Minimized;
+                this.WindowState = FormWindowState.Normal;
+                this.BringToFront();
+            }
+            else
+            {
+                //hide
+                button2_Click(sender, e);
+            }
+        }
+
+        void exit_Click(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            this.BringToFront();
+            this.ShowInTaskbar = true;
+            timer1.Enabled = false;
+            /*
+            if (driveDataClass != null)
+            {
+                driveDataClass.Dispose();
+            }
+             */
+            //Close();
+            removeTrayIcon();
+            Application.Exit();
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
+            //close -> hide
             this.WindowState = FormWindowState.Minimized;
             this.ShowInTaskbar = false;
+            this.Visible = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -353,6 +421,73 @@ namespace SSD_LED
             saveSettings();
         }
 
+        private void OnReadTimeOut(object sender, ElapsedEventArgs e)
+        {
+            readTimer.Enabled = false;
+            endReading = true;
+            readThread.Join();
+            readThread = null;
+            if (button4.InvokeRequired)
+            {
+                button4.Invoke((MethodInvoker)delegate { button4.Enabled = true; });
+            }
+            else
+            {
+                button4.Enabled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //read for x seconds
+            if (endReading == true)
+            {
+                button4.Enabled = false;
+                endReading = false;
+                readTimer.Enabled = true;
+                readThread = new Thread(delegate () { ReadSpeed(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)); });
+                readThread.Start();
+            }
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            maxSpeedKBS = trackBar1.Value;
+            textBox1.Text = maxSpeedKBS + " KB/s";
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar2.Value;
+            textBox2.Text = trackBar2.Value + " ms";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                comboBox1.Enabled = true;
+                if (diskSelectionPFCStr != null)
+                    foreach (var item in comboBox1.Items)
+                        if (item.ToString().Equals(diskSelectionPFCStr))
+                            comboBox1.SelectedItem = item;
+            }
+            else
+            {
+                comboBox1.Enabled = false;
+                diskSelectionPFCStr = null;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            diskSelectionPFCStr = GetInstanceNameByDriveIndex(comboBox1.SelectedIndex);
+            Debug.WriteLine("Selected: " + diskSelectionPFCStr);
+        }
+
+        #endregion
+
+        #region settings load and save
         private bool loadSettings()
         {
             int tempInt;
@@ -393,127 +528,12 @@ namespace SSD_LED
             }
             Properties.Settings.Default.Save();
         }
+        #endregion
 
-        private void ReadSpeed(string sDir)
-        {
-            /*
-                [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-                private static extern SafeFileHandle CreateFile(string lpFileName, [MarshalAs(UnmanagedType.U4)] FileAccess dwDesiredAccess, [MarshalAs(UnmanagedType.U4)] FileShare dwShareMode, IntPtr lpSecurityAttributes, [MarshalAs(UnmanagedType.U4)] FileMode dwCreationDisposition, [MarshalAs(UnmanagedType.U4)] FileAttributes dwFlagsAndAttributes, IntPtr hTemplateFile);
+        
 
-                [DllImport("kernel32.dll", SetLastError = true)]
-                [return: MarshalAs(UnmanagedType.Bool)]
-                private static extern bool CloseHandle(IntPtr hObject);
-                
-                FileOptions fileOptions = (FileOptions)(134217728 | (int.MinValue ) | (536870912));
-                SafeFileHandle file = CreateFile(f, FileAccess.Read, FileShare.ReadWrite, IntPtr.Zero, FileMode.Open, (FileAttributes)fileOptions, IntPtr.Zero);
-                if (file.IsInvalid)
-                    return;
-                FileStream fileStream = new FileStream(file, FileAccess.Read, 4096, false);
-            */
-            try
-            {
-                foreach (string f in Directory.GetFiles(sDir))
-                {
-                    byte[] buf = new byte[1024 * 1024];
-                    try
-                    {
-                        int offset = 0;
-                        while (endReading != false)
-                        {
-                            using (Stream file = File.OpenRead(f))
-                            {
-                                file.Read(buf, offset, 1024 * 1024);
-                                offset += 1024 * 1024;
-                            }
-                        }
-                    }
-                    catch (Exception) { };
-                    if (endReading)
-                        break;
-                }
-
-                foreach (string d in Directory.GetDirectories(sDir))
-                {
-                    if (endReading)
-                        break;
-                    ReadSpeed(d);
-                }
-            }
-            catch (System.Exception) { };
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            //read for x seconds
-            if (endReading == true)
-            {
-                button4.Enabled = false;
-                endReading = false;
-                readTimer.Enabled = true;
-                readThread = new Thread(delegate () { ReadSpeed(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)); });
-                readThread.Start();
-            }
-        }
-
-        private void trackBar1_ValueChanged(object sender, EventArgs e)
-        {
-            maxSpeedKBS = trackBar1.Value;
-            textBox1.Text = maxSpeedKBS + " KB/s";
-        }
-
-        private void RefreshDriveList()
-        {
-            comboBox1.Items.Clear();
-            /*
-            foreach (string logicalDrive in Environment.GetLogicalDrives)
-            {
-                DriveInfo driveInfo = new DriveInfo(logicalDrive);
-                if (driveInfo.IsReady)
-                {
-                    this.comboBox1.Items.Add((object)(logicalDrive.ToUpper() + " [" + driveInfo.DriveFormat.ToString() + "]"));//; " + (object)(driveInfo.TotalSize / 1024L / 1024L) + " / " + (object)(driveInfo.AvailableFreeSpace / 1024L / 1024L) + " MiB]"));
-                }
-            }
-             */
-            /*
-            foreach (System.IO.DriveInfo drive in System.IO.DriveInfo.GetDrives())
-            {
-                if (drive.DriveType == System.IO.DriveType.Fixed) 
-                {
-                    comboBox1.Items.Add(drive.Name.ToString());
-                }
-            }
-             */
-            PerformanceCounterCategory pfcCat = new PerformanceCounterCategory("PhysicalDisk");
-            comboBox1.Items.AddRange(pfcCat.GetInstanceNames());
-        }
-
-        private void trackBar2_ValueChanged(object sender, EventArgs e)
-        {
-            timer1.Interval = trackBar2.Value;
-            textBox2.Text = trackBar2.Value + " ms";
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            if (checkBox1.Checked)
-            {
-                comboBox1.Enabled = true;
-                if (diskSelectionPFCStr != null)
-                    foreach (var item in comboBox1.Items)
-                        if (item.ToString().Equals(diskSelectionPFCStr))
-                            comboBox1.SelectedItem = item;
-            }
-            else
-            {
-                comboBox1.Enabled = false;
-                diskSelectionPFCStr = null;
-            }
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            diskSelectionPFCStr = GetInstanceNameByDriveIndex(comboBox1.SelectedIndex);
-            Debug.WriteLine("Selected: " + diskSelectionPFCStr);
-        }
+        
     }
+
+    
 }
