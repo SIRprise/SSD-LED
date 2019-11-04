b0ab3307b827e97b3d0cd18e0f02f55d6845d47f
SIRprise <peter3@turboprinz.de>
2019-11-04 07:23:36 +0100

    added comment

--- SSD-LED/DiskActivity.cs
+++ SSD-LED/DiskActivity.cs
@@ -6,6 +6,7 @@ using System.Text;
 namespace SSD_LED
 {
 #warning TBD: refactoring by moving all disk technical stuff to here
+#warning TBD: add access time measurement to warn about busy/defective device: https://blogs.technet.microsoft.com/askcore/2012/02/07/measuring-disk-latency-with-windows-performance-monitor-perfmon/
     class DiskActivity
     {
     }
